e4a603f8c9056c884128f403cad7df873295503e
Haokang Den <haokang.den@gmail.com>
2019-07-16 12:45:10 +0900

    bump google-cloud-pubsub to 1.82.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Apache-2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [com.google.cloud/google-cloud-pubsub "1.78.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [com.google.cloud/google-cloud-pubsub "1.82.0"]]
   :java-source-paths ["src/java"]
   :profiles {:uberjar {:aot :all}})
